0b802c537bea5075a39a0e6954faf89574c48cbd
mustafa <mustafanaiby@gmail.com>
2023-02-16 15:16:37 +0330

    h

--- src/Components/counter.jsx
+++ src/Components/counter.jsx
@@ -2,30 +2,55 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+
+    myName: "MustafaJohn@Gmail.com",
+    Password: "@@Mustafa12345",
   };
   render() {
     return (
       <>
-        <button onClick={this.Handle} className="btn btn-sm btn-warning">
-          Decrease
-        </button>
-        <button onClick={this.Reset} className="btn btn-sm btn-warning">
-          {this.state.count}
-        </button>
-        <button onClick={this.Handler} className="btn btn-sm btn-warning">
-          Increase
-        </button>
+        <div className="container">
+          <button onClick={this.DecreaseHandler} className="btn btn-primary">
+            Decrease
+          </button>
+          <button onClick={this.ResetHandler} className="btn btn-warning">
+            {this.state.count}
+          </button>
+          <button onClick={this.IncreaseHandler} className="btn btn-primary">
+            Increase
+          </button>
+        </div>
+
+        <div className="btn">
+          <button className="btn btn-danger" onClick={this.Resetpwd}>
+            ChangeState
+          </button>
+          <button className=" one btn btn-danger" onClick={this.Alert}>
+            Alerbtn
+          </button>
+        </div>
+        <br />
+        <h2 className="text text-center">{this.state.myName}</h2>
+        <h2 className="text text-center">{this.state.Password}</h2>
       </>
     );
   }
-  Handler = () => {
-    this.setState({ count: this.state.count + 1 });
+
+  Alert = () => {
+    window.alert("Nice Shot");
   };
-  Reset = () => {
+
+  Resetpwd = () => {
+    this.setState({ Password: "StateChangedToThisByClick" });
+  };
+  DecreaseHandler = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+  ResetHandler = () => {
     this.setState({ count: (this.state.count = 0) });
   };
-  Handle = () => {
-    this.setState({ count: this.state.count - 1 });
+  IncreaseHandler = () => {
+    this.setState({ count: this.state.count + 1 });
   };
 }
 
